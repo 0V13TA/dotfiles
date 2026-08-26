84332502c4bb8750550a2ec9cc4e876781ec0ca2
0V13TA <vincentpaulomorogbe6@gmail.com>
2026-08-26 20:12:25 +0100

    Added an nvim theme

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,28 +1,40 @@
 return {
+  -- Corrected repository name:
   {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    "RRethy/base16-nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local colors_path = vim.fn.expand("~/.cache/wallust/base16.lua")
+      
+      -- Only apply if Wallust has generated the palette
+      if vim.uv.fs_stat(colors_path) then
+        local ok, colors = pcall(dofile, colors_path)
+        if ok and type(colors) == "table" then
+          require("base16-colorscheme").setup(colors)
+
+          -- Optional: Restore transparency if desired
+          vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+          vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+          
+          -- Remove the solid background from the top buffer/tab line
+          vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE" })
+          vim.api.nvim_set_hl(0, "TbLineFill", { bg = "NONE" })
+          
+        end
+      end
+    end,
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "stevearc/conform.nvim",
+    opts = require "configs.conform",
+  },
+  
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
 }
