ec904ff41be791fe4e2d2785e265a03530c181ca
0V13TA <vincentpaulomorogbe6@gmail.com>
2026-08-31 01:40:00 +0100

    feat: add glslls to neovim lspconfig

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,21 +1,41 @@
 -- Load NvChad default LSP settings/diagnostics/capabilities
 require("nvchad.configs.lspconfig").defaults()
 
--- 1. Standard web/JS servers
+-- Standard web/JS servers
 local servers = { "html", "cssls", "jsonls", "ts_ls", "gopls" }
 vim.lsp.enable(servers)
 
--- 2. Clangd (C / C++)
+-- Clangd (C / C++)
 vim.lsp.config("clangd", {
   cmd = { "/usr/bin/clangd" },
 })
 vim.lsp.enable("clangd")
 
--- 3. Rust Analyzer
+-- Rust Analyzer
 vim.lsp.config("rust_analyzer", {})
 vim.lsp.enable("rust_analyzer")
 
--- 4. Lua Language Server (Hyprland stubs integration)
+-- Odin Language Server
+vim.lsp.config("ols", {})
+vim.lsp.enable("ols")
+
+-- GLSLLS
+vim.lsp.config("glslls", {})
+vim.lsp.enable("glslls")
+vim.filetype.add({
+  extension = {
+    vert = 'glsl',
+    frag = 'glsl',
+    comp = 'glsl',
+    vs   = 'glsl', -- Raylib vertex shader
+    fs   = 'glsl', -- Raylib fragment shader
+    geom = 'glsl',
+    tesc = 'glsl',
+    tese = 'glsl',
+  }
+})
+
+-- Lua Language Server (Hyprland stubs integration)
 vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
