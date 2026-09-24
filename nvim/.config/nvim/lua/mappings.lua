18dc3bce364086fe94e6c9e3b7d2c87169f5581a
0V13TA <vincentpaulomorogbe6@gmail.com>
2026-09-24 20:28:47 +0100

    nvim stuffs

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -7,4 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+vim.keymap.set("i", "<A-h>", "<C-g>U<Left>")
+vim.keymap.set("i", "<A-j>", "<C-g>U<Down>")
+vim.keymap.set("i", "<A-k>", "<C-g>U<Up>")
+vim.keymap.set("i", "<A-l>", "<C-g>U<Right>")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,6 +1,8 @@
 require "nvchad.options"
 
 -- add yours here!
+vim.o.ttimeout = true
+vim.o.ttimeoutlen = 100
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
